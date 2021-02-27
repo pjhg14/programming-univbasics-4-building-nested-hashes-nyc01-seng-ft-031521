d3551c1685b26db631e67fcf2c0f99786696551d
Paul H Graham Jr <pjhg14@gmail.com>
2021-02-27 01:56:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
@@ -10,7 +12,9 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   hash = {railroads: {}}
+  binding.pry
   hash[:railroads] = {pieces: "4", rent_in_dollars: {}, names: {}}
+  binding.pry
 end
 
 def monopoly_with_third_tier
